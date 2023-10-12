6d8c005c533a0e4ff40ae4469945d0af83a43076
Juan <juanpmad@outlook.com>
2023-10-12 18:40:49 -0300

    controller

---
+++ src/main/java/io/github/juanmadeira/todolist/controller/controller.java
@@ -0,0 +1,13 @@
+package io.github.juanmadeira.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+
+public class controller {
+    
+    // método (funcionalidade) de uma classe
+    public String message() {
+        return "teste";
+    }
+}
